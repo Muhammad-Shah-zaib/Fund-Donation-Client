f89111c99e455b9920b89000f06e02a5de78a9d9
Pace_Setter <132779528+Muhammad-Shah-zaib@users.noreply.github.com>
2024-06-03 23:52:39 +0500

    made cause description functional

--- src/Services/CauseBankService.ts
+++ src/Services/CauseBankService.ts
@@ -1,6 +1,6 @@
 import { delay, Observable } from "rxjs";
-import { getAllCausesUrl } from "../Environment/ServerUrls";
-import { CauseResponseListDto } from "../Models/CauseResponseDto";
+import { getAllCausesUrl, getCauseByIdUrl } from "../Environment/ServerUrls";
+import { CauseResponseDto, CauseResponseListDto } from "../Models/CauseResponseDto";
 import { ajax } from "rxjs/ajax";
 
 export default class CauseBankService{
@@ -8,5 +8,10 @@ export default class CauseBankService{
     public GetAllCauses$(): Observable<CauseResponseListDto> {
         return ajax.getJSON<CauseResponseListDto>(getAllCausesUrl)
             .pipe(delay(200));
+    }
+
+    public GetCauseById$(id: number): Observable<CauseResponseDto> {
+        return ajax.getJSON<CauseResponseDto>(getCauseByIdUrl + id)
+            .pipe();
     }
 }
